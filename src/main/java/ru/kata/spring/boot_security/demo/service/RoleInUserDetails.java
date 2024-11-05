80af6d463e016b52977388ef779f913a2d1d7c50
Anton200021243 <mortal0011@gmail.com>
2024-11-05 18:03:19 +0300

    some clear code

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleInUserDetails.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleInUserDetails.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
 @Service
