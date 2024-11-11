c2098abd7c832165b532b5800c7e374873d6d2f0
Anton200021243 <mortal0011@gmail.com>
2024-11-11 16:22:38 +0300

    getAllRoles moved in RestAdminController

--- src/main/java/ru/kata/spring/boot_security/demo/controller/RestAdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RestAdminController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.util.List;
@@ -15,11 +17,13 @@ import java.util.List;
 public class RestAdminController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final RoleService roleService;
 
     @Autowired
-    public RestAdminController(UserService userService, PasswordEncoder passwordEncoder) {
+    public RestAdminController(UserService userService, PasswordEncoder passwordEncoder, RoleService roleService) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.roleService = roleService;
     }
 
     @GetMapping
@@ -50,5 +54,10 @@ public class RestAdminController {
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return new ResponseEntity<>(roleService.getAllRoles(), HttpStatus.OK);
     }
 }
